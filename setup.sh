fd580efe2918bc5e9ff4839c99432ac5983b0a18
Jonathan Reeve <jon.reeve@gmail.com>
2014-07-27 14:03:11 -0400

    back up existing files first

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
+mkdir bak
+mv ~/.bashrc bak/
+mv ~/.vim bak/
+mv ~/.vimrc bak/
+mv ~/.tmux.conf bak/
+mv ~/.vimperatorrc bak/
+mv ~/.pentadactylrc bak/
+mv ~/.inputrc bak/
+mv ~/.gitconfig bak/
 ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.vimrc ~/.vimrc
-#ln -s $PWD/.vimperatorrc ~/.vimperatorrc
-#ln -s $PWD/.pentadactylrc ~/.pentadactylrc
+ln -s $PWD/.vimperatorrc ~/.vimperatorrc
+ln -s $PWD/.pentadactylrc ~/.pentadactylrc
 ln -s $PWD/.tmux.conf-child ~/.tmux.conf
 ln -s $PWD/.inputrc ~/.inputrc
 ln -s $PWD/.gitconfig ~/.gitconfig
