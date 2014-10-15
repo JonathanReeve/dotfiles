84a206bedeb023c300c0bf01c188d05af13f8301
Jonathan Reeve <jon.reeve@gmail.com>
2014-10-15 18:42:25 -0400

    add initial i3 confs

--- setup.sh
+++ setup.sh
@@ -3,17 +3,23 @@ mv ~/.bashrc bak/
 mv ~/.vim bak/
 mv ~/.vimrc bak/
 mv ~/.tmux.conf bak/
-mv ~/.vimperatorrc bak/
-mv ~/.pentadactylrc bak/
 mv ~/.inputrc bak/
 mv ~/.gitconfig bak/
+mv ~/.i3/config bak/
 ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.vimrc ~/.vimrc
-ln -s $PWD/.vimperatorrc ~/.vimperatorrc
-ln -s $PWD/.pentadactylrc ~/.pentadactylrc
 ln -s $PWD/.tmux.conf-child ~/.tmux.conf
 ln -s $PWD/.inputrc ~/.inputrc
 ln -s $PWD/.gitconfig ~/.gitconfig
 git submodule update --init --recursive
-sudo apt-get install vim-nox weechat markdown pandoc php-codesniffer
+sudo apt-get install vim weechat markdown pandoc php-codesniffer 
+
+# Uncomment for GUI systems
+#mv ~/.vimperatorrc bak/
+#mv ~/.pentadactylrc bak/
+#ln -s $PWD/.vimperatorrc ~/.vimperatorrc
+#ln -s $PWD/.pentadactylrc ~/.pentadactylrc
+#mv ~/.i3/config bak/
+#ln -s $PWD/.i3/config ~/.i3/config
+#sudo apt-get install i3 kupfer chromium-browser ubuntu-restricted-extras firefox
