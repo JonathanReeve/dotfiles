d5f21b3563ad7d62e5408cd987f7bbcab2f38ddf
Jonathan Reeve <jon.reeve@gmail.com>
2015-01-27 12:29:38 -0500

    better setup script

--- setup.sh
+++ setup.sh
@@ -1,25 +1,40 @@
-#mkdir bak
+mkdir -p bak
 mv ~/.bashrc bak/
 mv ~/.tmux.conf bak/
 mv ~/.inputrc bak/
 mv ~/.gitconfig bak/
 mv ~/.i3/config bak/
+mv ~/.zshrc bak/
 ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.tmux.conf-child ~/.tmux.conf
 ln -s $PWD/.inputrc ~/.inputrc
 ln -s $PWD/.gitconfig ~/.gitconfig
-
+ln -s $PWD/.zshrc ~/.zshrc
 source ~/.bashrc 
 
-#sudo yum install vim weechat markdown pandoc php-codesniffer git 
-sudo apt-get install weechat markdown pandoc php-codesniffer git 
+# Determine Linux version 
+OS=$(lsb_release -si)
+if [ $OS == "Ubuntu" ]
+then 
+	export INSTALL='sudo apt-get install' 
+fi
+if [ $OS == "Fedora" ] 
+then 
+	export INSTALL='sudo yum install' 
+fi
+
+#Install Essential Packages 
+$INSTALL vim git 
+
+# Install Other Packages
+$INSTALL markdown pandoc php-codesniffer 
 
 # set up vim environment
 mv ~/.vim bak/
 mv ~/.vimrc bak/
 ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.vimrc ~/.vimrc
-sudo apt-get install vim
+$INSTALL vim
 
 # get vundle and other submodules
 git submodule update --init --recursive
@@ -29,14 +44,18 @@ vim -c PluginInstall
 #mv scripts/todo/todo.cfg bak/
 #ln -s $PWD/todo.cfg scripts/todo/todo.cfg 
 
-# Uncomment for GUI systems
-#mv ~/.vimperatorrc bak/
-#mv ~/.pentadactylrc bak/
-#ln -s $PWD/.vimperatorrc ~/.vimperatorrc
-#ln -s $PWD/.pentadactylrc ~/.pentadactylrc
-#mv ~/.i3/config bak/
-#ln -s $PWD/.i3/config ~/.i3/config
-#sudo yum install i3 kupfer chromium-browser vim-gnome ttf-anonymous-pro
-
+# Check for GUI
+if [[ $DISPLAY ]]
+then
+	mv ~/.vimperatorrc bak/
+	mv ~/.pentadactylrc bak/
+	mv ~/.gvimrc bak/
+	mv ~/.i3/config bak/
+	ln -s $PWD/.vimperatorrc ~/.vimperatorrc
+	ln -s $PWD/.pentadactylrc ~/.pentadactylrc
+	ln -s $PWD/.gvimrc ~/.gvimrc
+	ln -s $PWD/.i3/config ~/.i3/config
+	$INSTALL i3 kupfer chromium-browser vim-gnome ttf-anonymous-pro
+fi 
 #sudo apt-get install dwb
 #ln -s $PWD/.dwb/* ~/.config/dwb/
