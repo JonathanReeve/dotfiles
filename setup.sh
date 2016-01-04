ece736e3bf74f58f9b65531c80aefec2b698d577
Jonathan Reeve <jon.reeve@gmail.com>
2016-01-03 20:10:09 -0500

    tweaks to setup

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.inputrc ~/.inputrc
 ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.zshrc ~/.zshrc
-ln -S $PWD/.uzbl-config ~/.config/uzbl/config
+ln -s $PWD/.uzbl-config ~/.config/uzbl/config
 source ~/.bashrc 
 
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]
@@ -21,7 +21,13 @@ else
 fi
 
 # Determine Linux version 
-OS=$(lsb_release -si)
+if [ -f "/etc/arch-release" ]; then
+	OS="Arch"
+else
+	OS=$(lsb_release -si)
+fi
+
+# Get Linux install commands. 
 if [ $OS == "Ubuntu" ] || [ $OS == "Debian" ]
 then 
 	export INSTALL='sudo apt-get install' 
@@ -29,6 +35,10 @@ fi
 if [ $OS == "Fedora" ] 
 then 
 	export INSTALL='sudo yum install' 
+fi
+if [ $OS == "Arch" ] 
+then
+	export INSTALL='sudo pacman -S'
 fi
 
 # DOTFILES environment variable needed by .zshrc
@@ -40,17 +50,24 @@ $INSTALL vim git zsh
 chsh -s /bin/zsh
 
 # Install Other Packages
-$INSTALL markdown pandoc php-codesniffer 
+$INSTALL markdown pandoc
 
-# set up vim environment
+# Set up vim environment
 mv ~/.vim bak/
 mv ~/.vimrc bak/
 ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.vimrc ~/.vimrc
 $INSTALL vim
 
+# Set up neovim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+sudo ln -s $HOME/.vimrc /home/root/.vimrc
+sudo ln -s /home/root/.vimrc /home/.config/nvim/init.vim
 
-# get vundle and other submodules
+# Get vundle and other submodules
 git submodule update --init --recursive
 vim -c PluginInstall
 
@@ -59,9 +76,8 @@ vim -c PluginInstall
 #ln -s $PWD/todo.cfg scripts/todo/todo.cfg 
 #chmod +x scripts/todo-plugins/.todo.actions.d/birdseye
 
-#Install ZSH
+# Install ZSH
 $INSTALL zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
  
 # Check for GUI
@@ -69,13 +85,13 @@ if [[ $DISPLAY ]]
 then
 	mv ~/.vimperatorrc bak/
 	mv ~/.pentadactylrc bak/
-	mv ~/.gvimrc bak/
 	mv ~/.i3/config bak/
 	ln -s $PWD/.vimperatorrc ~/.vimperatorrc
 	ln -s $PWD/.pentadactylrc ~/.pentadactylrc
-	ln -s $PWD/.gvimrc ~/.gvimrc
 	ln -s $PWD/.i3/config ~/.i3/config
-	$INSTALL i3 kupfer vim-gnome ttf-anonymous-pro
+	$INSTALL i3
+	#neovim stuff
+	$INSTALL neovim python-neovim python2-neovim xsel 
 fi 
 #sudo apt-get install dwb
 #ln -s $PWD/.dwb/* ~/.config/dwb/
