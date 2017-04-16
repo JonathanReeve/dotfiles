3672e2ca60f8d12876b2d4bd6e33074dc213d9e4
Jonathan Reeve <jon.reeve@gmail.com>
2017-04-16 19:28:37 -0400

    remove t-recur

--- scripts/t-recur.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Just a wrapper for `t recur`
-export DOTFILES=/home/jon/Documents/Settings/dotfiles
-export TODO_DIR=/home/jon/Dropbox/Personal/Todo
-$DOTFILES/scripts/todo-plugins/.todo.actions.d/recur
