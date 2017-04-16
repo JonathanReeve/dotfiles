bcf108288a62f6ca3872a8d9fdcafb696952ce72
Jonathan Reeve <jon.reeve@gmail.com>
2017-04-16 19:28:16 -0400

    todo.sh recur scripts

---
+++ scripts/t-recur.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Just a wrapper for `t recur`
+export DOTFILES=/home/jon/Documents/Settings/dotfiles
+export TODO_DIR=/home/jon/Dropbox/Personal/Todo
+$DOTFILES/scripts/todo-plugins/.todo.actions.d/recur
