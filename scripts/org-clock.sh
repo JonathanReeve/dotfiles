11a667bf26c6091549c13f9e1d651e6e5bd25899
Jonathan Reeve <jon.reeve@gmail.com>
2017-11-02 17:36:15 -0400

    add emacs clock script

---
+++ scripts/org-clock.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/fish
+
+set CLOCKSTRING (emacsclient --eval '(if (org-clocking-p)(org-clock-get-clock-string)(message "0"))' 2>&1 )
+
+#echo $CLOCKSTRING
+
+switch "$CLOCKSTRING"
+case \"0\"
+    echo " Off-clock!"
+case "*can\'t find socket*"
+    echo " Emacs off!"
+case "*server-start*"
+    echo " Emacs off!"
+case "*ERROR*"
+    echo " Off-clock!"
+case "*function definition*"
+    echo " Off-clock!"
+case "*"
+    echo (echo $CLOCKSTRING | cut -d\" -f2)
+end
