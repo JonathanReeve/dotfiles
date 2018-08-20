0d23995ccb550e800ef2fb7f65912ea707ecc571
Jonathan Reeve <jon.reeve@gmail.com>
2018-08-20 18:16:28 -0400

    back to 10pt font in qb

--- dotfiles/qutebrowser.py
+++ dotfiles/qutebrowser.py
@@ -718,39 +718,39 @@ c.colors.tabs.selected.odd.bg = '#285577'
 
 ## Font used in the completion categories.
 ## Type: Font
-c.fonts.completion.category = '5pt monospace'
+c.fonts.completion.category = '10pt monospace'
 
 ## Font used in the completion widget.
 ## Type: Font
-c.fonts.completion.entry = '5pt monospace'
+c.fonts.completion.entry = '10pt monospace'
 
 ## Font used for the debugging console.
 ## Type: QtFont
-c.fonts.debug_console = '5pt monospace'
+c.fonts.debug_console = '10pt monospace'
 
 ## Font used for the downloadbar.
 ## Type: Font
-c.fonts.downloads = '5pt monospace'
+c.fonts.downloads = '10pt monospace'
 
 ## Font used for the hints.
 ## Type: Font
-c.fonts.hints = 'bold 5pt monospace'
+c.fonts.hints = 'bold 10pt monospace'
 
 ## Font used in the keyhint widget.
 ## Type: Font
-c.fonts.keyhint = '5pt monospace'
+c.fonts.keyhint = '10pt monospace'
 
 ## Font used for error messages.
 ## Type: Font
-c.fonts.messages.error = '5pt monospace'
+c.fonts.messages.error = '10pt monospace'
 
 ## Font used for info messages.
 ## Type: Font
-c.fonts.messages.info = '5pt monospace'
+c.fonts.messages.info = '10pt monospace'
 
 ## Font used for warning messages.
 ## Type: Font
-c.fonts.messages.warning = '5pt monospace'
+c.fonts.messages.warning = '10pt monospace'
 
 ## Default monospace fonts. Whenever "monospace" is used in a font
 ## setting, it's replaced with the fonts listed here.
@@ -759,15 +759,15 @@ c.fonts.monospace = 'Hack, "Input Mono", "xos4 Terminus", Terminus, Monospace, "
 
 ## Font used for prompts.
 ## Type: Font
-c.fonts.prompts = '5pt sans-serif'
+c.fonts.prompts = '10pt sans-serif'
 
 ## Font used in the statusbar.
 ## Type: Font
-c.fonts.statusbar = '5pt monospace'
+c.fonts.statusbar = '10pt monospace'
 
 ## Font used in the tab bar.
 ## Type: QtFont
-c.fonts.tabs = '5pt monospace'
+c.fonts.tabs = '10pt monospace'
 
 ## Font family for cursive fonts.
 ## Type: FontFamily
