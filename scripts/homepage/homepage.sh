18476d524ccf512069a023048f210016a802faf8
Jonathan Reeve <jon.reeve@gmail.com>
2019-05-09 10:48:39 -0400

    theme homepage with pywal colors

--- scripts/homepage/homepage.sh
+++ scripts/homepage/homepage.sh
@@ -5,4 +5,6 @@ set id "c8c13cd9-1ab1-4f48-afb6-9f48f0b38002"
 emacsclient -e "(progn (find-file \"$file\" ) \
                        (org-id-goto \"$id\") \
                        (org-html-export-to-html nil t nil t))"
-pandoc -f html -t html --template=template-pandoc.html --metadata pagetitle="Homepage" -o $PWD/homepage.html $out
+set bg (head ~/.cache/wal/colors -n 2 | tail -n 1)
+set fg (head ~/.cache/wal/colors -n 3 | tail -n 1)
+pandoc -f html -t html --template=template-pandoc.html --metadata pagetitle="Homepage" -V fg=$fg -V bg=$bg -o $PWD/homepage.html $out
