6527d6ca6c3f7dde57a9fecba8cabc337b34aeed
Jonathan Reeve <jon.reeve@gmail.com>
2019-05-15 21:04:15 -0400

    colorscheme changes

--- scripts/homepage/homepage.sh
+++ scripts/homepage/homepage.sh
@@ -5,6 +5,6 @@ set id "c8c13cd9-1ab1-4f48-afb6-9f48f0b38002"
 emacsclient -e "(progn (find-file \"$file\" ) \
                        (org-id-goto \"$id\") \
                        (org-html-export-to-html nil t nil t))"
-set bg (head ~/.cache/wal/colors -n 2 | tail -n 1)
-set fg (head ~/.cache/wal/colors -n 3 | tail -n 1)
+set bg (head ~/.cache/wal/colors -n 1 | tail -n 1)
+set fg (head ~/.cache/wal/colors -n 2 | tail -n 1)
 pandoc -f html -t html --template=template-pandoc.html --metadata pagetitle="Homepage" -V fg=$fg -V bg=$bg -o $PWD/homepage.html $out
