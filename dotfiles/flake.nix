0da35d7dafbd2a3a172b1726f26dc25735723f19
Jonathan Reeve <jon.reeve@gmail.com>
2021-03-07 23:41:10 -0500

    add flake.nix

---
+++ dotfiles/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "My NixOS configuration.";
+  inputs = {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixos";
+    };
+    nixos.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixos, home-manager }: {
+     nixosConfigurations.jon-laptop = nixos.lib.nixosSystem {
+       system = "x86_64-linux";
+       modules = [ ./configuration.nix
+                   home-manager.nixosModules.home-manager {
+                     home-manager.useGlobalPkgs = true;
+                     home-manager.useUserPackages = true;
+                     home-manager.users.user = import ./home.nix;
+                   }
+                 ];
+     };
+  };
+}
