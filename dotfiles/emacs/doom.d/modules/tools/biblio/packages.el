c72cc27c2cd7f351c1860dc277d01da33dd8c078
Jonathan Reeve <jon.reeve@gmail.com>
2021-03-20 21:20:11 -0400

    put custom module in modules dir

--- dotfiles/emacs/doom.d/modules/tools/biblio/packages.el
+++ dotfiles/emacs/doom.d/modules/tools/biblio/packages.el
@@ -7,6 +7,6 @@
 (when (featurep! :completion helm)
   (package! helm-bibtex :pin "ca09076c3d6e51cc4ffe208c8176fccf3710fcc6"))
 (when (featurep! :lang org)
-  (package! org-ref :pin "93f17b8a2cc9048a8df576f189cd5263662168db"))
+  (package! org-ref :pin "7b0ebdd2e34b3a509c7f043cb9d919a0069491c2"))
 (when (featurep! :lang org +roam)
-  (package! org-roam-bibtex :pin "068d9c27650207447ecf7a41fcd5b7abc40f4c3c"))
+  (package! org-roam-bibtex :pin "81b6fedf99996a78199067e61935964dea9389ee07447ecf7a41fcd5b7abc40f4c3c"))
