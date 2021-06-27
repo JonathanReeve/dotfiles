799659b758897ed657f2fa89b2d95c47cfa8b9e3
Jonathan Reeve <jon.reeve@gmail.com>
2021-06-27 11:11:21 -0400

    override nix packages; add org-roam config

--- dotfiles/flake.nix
+++ dotfiles/flake.nix
@@ -25,6 +25,26 @@
                          enable = true;
                          # dependencyOverrides =
                          doomPrivateDir = ./emacs/doom.d;
+			 emacsPackagesOverlay = self: super: {
+         # Use experimental versions of org-roam and org-roam-bibtex.
+         # I wish I could just do this in my doom config, but nix-straight.el doesn't seem to respect recipes.
+				 org-roam = super.org-roam.overrideAttrs (esuper: { 
+				    src = pkgs.fetchFromGitHub {
+					    owner = "jethrokuan";
+					    repo = "org-roam";
+					    rev = "8bed015cdff44bc813cad5c9cbe0194026d53370";
+					    sha256 = "iicx/zsZIk/5ivy5xn3HW03JBCO4ascVdgc5qzu3yww=";
+					    };
+				    });
+				 org-roam-bibtex = super.org-roam-bibtex.overrideAttrs (esuper: {
+				    src = pkgs.fetchFromGitHub {
+					    owner = "org-roam";
+					    repo = "org-roam-bibtex";
+					    rev = "c4d3afcb3143cd7db30589c81110f677510c1bf9";
+					    sha256 = "iicx/zsZIk/5ivy5xn3HW03JBCO4ascVdgc5qzu3yww=";
+					    };
+				    });
+				 };
                          extraPackages = with pkgs; [ mu pass gnupg ];
                          extraConfig = ''
                           (setq mu4e-mu-binary "${pkgs.mu}/bin/mu")
