7c4a4572aab84a68e6b8e0a2342601f8d6cc598c
Jonathan Reeve <jonathan.reeve@columbia.edu>
2021-11-15 19:46:48 -0500

    add cachix files

---
+++ dotfiles/cachix/digitallyinduced.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://digitallyinduced.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE="
+    ];
+  };
+}
+    
---
+++ dotfiles/cachix/miso-haskell.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://miso-haskell.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8="
+    ];
+  };
+}
+    
